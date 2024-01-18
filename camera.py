606802820afeb9d217ab7693840a0742e4e765b3
Maull09 <maulanafirdaus63@gmail.com>
2024-01-18 22:39:11 +0700

    add test ml

--- camera.py
+++ camera.py
@@ -1,45 +1,36 @@
 import cv2
+from detection import AccidentDetectionModel
+import numpy as np
 import os
+from PIL import Image
+import send_mail
+import random
 from ultralytics import YOLO
-import cv2
 
+model = YOLO("best.pt")
+font = cv2.FONT_HERSHEY_SIMPLEX
 url = "https://103.164.218.114/camera/share/tios/2/25/index.m3u8"
-source_video = "./api/static/video.mp4"
+source_video = "video.mp4"
 
-def startapplication(location:str):
-    
-    model = YOLO("./api/routes/best.pt")
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    video = cv2.VideoCapture(url) # for camera use video = cv2.VideoCapture(0)
+def startapplication():
+    video = cv2.VideoCapture(source_video) # for camera use video = cv2.VideoCapture(0)
+    save_count = 0
+    alert_counter = 0
     frame_counter = 0
     frame_list = []
-    video_folder = ".api/static/video"
+    save_folder = "./image"
+    video_folder = "./video1"
+    video_output_path = os.path.join(video_folder, f'crash_alert_{save_count}.mp4')
 
-    # create video path
-    count = 0
-    video_path = f'{location}_{count}'
-    while os.path.isfile(os.path.join(video_folder, f'{video_path}.mp4')):
-        count += 1
-        video_path = f'{location}_{count}'
-    video_output_path = os.path.join(video_folder, f'{video_path}.mp4')
-    return video_output_path # Temporary code
-
+    if not os.path.exists(save_folder):
+        os.makedirs(save_folder)
     if not os.path.exists(video_folder):
         os.makedirs(video_folder)
 
-    ret = True
-    i = 0 # temporary code
-    while ret:
-    
-        # temporary code
-        i+=1
-        if (i == 100):
-            print("break")
-            break
-
+    while True:
         ret, frame = video.read()
         results = model.predict(frame)
-
+        result = results[0]
         # Process results list
         boxes = results[0].boxes.xywh.cpu()                        #xywh bbox list
         clss = results[0].boxes.cls.cpu().tolist()                 #classes Id list
@@ -56,14 +47,24 @@ def startapplication(location:str):
             if (str(names[cls]) == "car-crash" or str(names[cls]) == "car-crashs") and round(conf, 2) > 0.7:
                 frame_list.append(frame)  # Tambahkan frame ke daftar
                 frame_counter += 1
+                print(frame_counter)
             else:
                 if len(frame_list) > 30 : 
                     frame_height, frame_width, _ = frame_list[0].shape
+                    video_output_path = os.path.join(video_folder, f'crash_alert_{save_count}.mp4')
                     video_writer = cv2.VideoWriter(video_output_path, cv2.VideoWriter_fourcc(*"mp4v"), 15.0,
                                                 (frame_width, frame_height))
                     for saved_frame in frame_list:
                         video_writer.write(saved_frame)
                     video_writer.release()
-                    return video_path
-    return False
-        
+                    # send_mail.SendVideo(video_output_path)
+                    save_count += 1
+                    frame_list = []  # Reset frame list after video is created
+            
+        if cv2.waitKey(33) & 0xFF == ord('q'):
+            return
+        cv2.imshow('Video', frame) 
+
+
+if __name__ == '__main__':
+    startapplication()
--- src/app/unsupported/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Unsupported() {
-  return <div>Unsupported Device</div>;
-}
-
-export default Unsupported;
